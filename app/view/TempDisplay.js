6b7f9b81966bf19f9dc843de6a62ed66b72da9eb
Ken Brown <kenneth.brown@e-hps.com>
2013-06-07 16:19:37 -0500

    Moved margins to style

--- app/view/TempDisplay.js
+++ app/view/TempDisplay.js
@@ -20,14 +20,12 @@ Ext.define('MyAppName.view.TempDisplay', {
                 	xtype: 'panel',
                 	baseCls: 'temperaturedisplay',
                     flex: 1,
-                  	margin: '0 00 10 0',
                 	html: '85'
                 },
                 {
                     xtype: 'panel',
                     baseCls: 'temperaturelabeldisplay',
                     docked: 'right',
-                  	margin: '25 20 10 5',
                     html: '&#176F'
                 }
             ]
